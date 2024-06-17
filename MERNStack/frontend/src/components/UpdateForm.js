b160a35797aabe2a8f10d955620b1c87464956bb
qInTheMiddle <154910485+qInTheMiddle@users.noreply.github.com>
2024-06-17 09:18:26 +0300

    Update UpdateForm.js

--- MERNStack/frontend/src/components/UpdateForm.js
+++ MERNStack/frontend/src/components/UpdateForm.js
@@ -7,8 +7,8 @@ const UpdateForm = ({ branch, onClose }) => {
     const [location, setLocation] = useState(branch.location);
     const [performanceScore, setPerformanceScore] = useState(branch.performanceScore);
 
-    const handleSubmit = async (b) => {
-        b.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
         const updatedBranchData = {
             name,
@@ -47,11 +47,11 @@ const UpdateForm = ({ branch, onClose }) => {
       <h2>Update Branch</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={name} onChange={(b) => setName(b.target.value)} />
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
         <label htmlFor="location">Location:</label>
-        <input type="text" id="location" value={location} onChange={(b) => setLocation(b.target.value)} />
+        <input type="text" id="location" value={location} onChange={(e) => setLocation(e.target.value)} />
         <label htmlFor="performanceScore">Performance Score:</label>
-        <input type="number" id="performanceScore" value={performanceScore} onChange={(b) => setPerformanceScore(b.target.value)} />
+        <input type="number" id="performanceScore" value={performanceScore} onChange={(e) => setPerformanceScore(e.target.value)} />
         <button type="submit">Update</button>
         <button type="button" onClick={onClose}>Cancel</button>  
         {error && <div className="error">{error}</div>} {/* Utilizing error */}
